6add7dc0c4e756c2c82175abd2f20107d4d61293
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-10 10:24:31 +0900

    * add express app.

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on localhost port ${PORT}`);
+});
