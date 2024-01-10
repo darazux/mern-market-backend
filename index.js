da658956dfb69d65d383de61322311cb4d9aff72
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2024-01-10 10:29:05 +0900

    * add GET test.

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  return res.status(200).json('hello');
+});
+
+// ITEM functions
+// Create Item
+// Read All Items
+// Read Single Item
+// Update Item
+// Delete Item
+
+// USER functions
+// Register User
+// Login User
+
 app.listen(PORT, () => {
   console.log(`Listening on localhost port ${PORT}`);
 });
